08811c68fc789a826a1af83367d380f260ea33d5
Craig Wendel <wendelcraig@gmail.com>
2017-06-14 17:09:56 -0500

    Add JS to make functions work and evaluate. Add CSS to make calculator responsive on mobile. Add minor HTML tweaks.

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
 //variables:
-let clearButton = document.getElementById('clear-button');
-let screenDisplay = document.getElementById('screen');
+let clearButton = document.querySelector('#clear-button');
+let screenDisplay = document.querySelector('#screen');
 let getButtons = document.querySelectorAll('.buttons h3');
-let product = document.querySelectorById('multiply')
-let quotient = document.querySelectorById('divide');
-let addition = document.querySelectorById('add');
-let subtraction = document.querySelectorById('subtract');
-let evaluation = document.querySelectorById('equal');
+let product = document.querySelector('#multiply')
+let quotient = document.querySelector('#divide');
+let addition = document.querySelector('#add');
+let subtraction = document.querySelector('#subtract');
+let evaluation = document.querySelector('#equal');
+
 
 clearButton.addEventListener('click', function(){
-  screenDisplay.textContent = " ";
-  calculate = [];
+screenDisplay.textContent = " ";
+calculate = [];
 });
 
+product.addEventListener('click', function() {
+calculate.push('*');
+screenDisplay.textContent = calculate.join("");
+});
+
+quotient.addEventListener('click', function() {
+calculate.push('/');
+screenDisplay.textContent = calculate.join("");
+});
+
+addition.addEventListener('click', function() {
+calculate.push('+');
+screenDisplay.textContent = calculate.join("");
+});
+
+subtraction.addEventListener('click', function() {
+calculate.push('-');
+screenDisplay.textContent = calculate.join("");
+});
+
+
+evaluation.addEventListener('click', function() {
+let finalCalc = calculate.join('');
+finalCalc = eval(finalCalc);
+screenDisplay.textContent = finalCalc;
+});
 
 var calculate = [];
 
@@ -25,5 +52,4 @@ for (let i = 0; i < getButtons.length; i++) {
      console.log(calculate);
      screenDisplay.textContent = calculate.join("");
   })
-
 };
